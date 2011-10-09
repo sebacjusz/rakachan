8497f326d704c26716393c5d5509d23e306d6ac8
Sebacjusz <sovietmudkipz22@gmail.com>
2011-10-09 21:22:04 +0200

    add static layout files

---
+++ static/rakachan.js
@@ -0,0 +1,9 @@
+
+function ppv(t, board,id){
+	$("<div id='preview' style='position: fixed'></div>").appendTo("body");
+	$('#preview').load('/alfa/post/' + board + '/' + id);
+	var position = $(t).position(); 
+	$('#preview').css('top', parseInt(position.top) - window.pageYOffset); 
+	i = parseInt(position.left) + parseInt($(t).css('width'));
+	$('#preview').css('left', i);
+}
