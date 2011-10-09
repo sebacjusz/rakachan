eea0ed625f017f6a5db8feab894d03486d83029b
Sebacjusz <sovietmudkipz22@gmail.com>
2011-10-09 22:34:37 +0200

    flask migration - routing

--- czan.py
+++ czan.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-#import cProfile
-#from os import path
 import time, urlparse, re, sys
 from flup.server.fcgi import WSGIServer
 from jinja2 import Environment, FileSystemLoader
+from flask import Flask, request
 import pymongo
 j_env = Environment(loader=FileSystemLoader('./'), trim_blocks=True)
 db_conn = pymongo.Connection()
 posts_coll = db_conn.kara2.posts
 #txt_fields=['name', 'tripcode', 'email', 'subject', 'file', 'file_original', 'message']
 conf={'www_path':'/czan', 'anonymous': '[Anon]'}
+
+app=Flask(__name__)
+app.debug=True
 
 def xlink_filter(msg, def_board):
     if not msg:
@@ -31,72 +33,50 @@ def xlink_filter(msg, def_board):
 j_env.filters['mkxlink'] = xlink_filter
 j_env.filters['nl2br'] = lambda x: x.replace('\n', '<br />')
 
-def search1(environ, path, start_response):
-    query=urlparse.parse_qs(environ['QUERY_STRING'])
+@app.route('/search')
+def search1():
     tpl=j_env.get_template('post.html')
-    q = query.get('q', ['.*wykop.*'])[0].decode('utf-8')
-    postst = posts_coll.find( { 'message' : { '$regex' : q } } ).limit(100)
-    sc = postst.count()
-    ret=tpl.render(posts=postst, mode={'search':1, 'wrap':1}, config=conf, search={'count':sc, 'q':q} )
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return [ret.encode('utf-8')]
-def read_single(environ, path, start_response):
-    tpl=j_env.get_template('post.html')
-    try:
-        r_id = int( path[2] )
-        r_board = path[1] 
-    except (IndexError, ValueError, TypeError):
-        start_response('400 Bad Request', [('Content-Type', 'text/html')])
-        return ["Invalid post ID."]
-    post = posts_coll.find_one( {'id' : r_id, 'board': r_board } ) 
-    if post is None:
-        start_response('404 Not Found', [('Content-Type', 'text/html')])
-        return ["<h1>404</h1><br> A post with this id doesn't exist."]
-    #del post['file']
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return [tpl.render(posts=[post], config=conf, mode={'single':1}).encode('utf-8')]
+    q=request.args.get('q', '')
+    if q:
+        postst = posts_coll.find( { 'message' : { '$regex' : q } } ).limit(100)
+        sc = postst.count()
+        search={'count':sc, 'q':q}
+    else:
+        postst=[]
+        search=None
+    ret=tpl.render(posts=postst, mode={'search':1, 'wrap':1}, config=conf, search=search)
+    return ret
 
-def read_thread(environ, path, start_response):
+@app.route('/post/<p_board>/<int:p_id>')
+def read_single(p_board, p_id):
     tpl=j_env.get_template('post.html')
-    try:
-        board = path[1].decode('utf-8')
-        t_id = int( path[2] )
-    except (IndexError, ValueError, TypeError):
-        start_response('400 Bad Request', [('Content-Type', 'text/html')])
-        return ["Invalid thread ID."]
-    OP = posts_coll.find_one( {'board' : board, 'thread':0, 'id': t_id } )
+    post = posts_coll.find_one( {'id' : p_id, 'board': p_board } ) 
+    if post is None:
+        return "<h1>404</h1><br> A post with this id doesn't exist. (id %d, brd %s)" % (p_id, p_board), 404
+    return tpl.render(posts=[post], config=conf, mode={'single':1})
+
+@app.route('/thread/<t_board>/<int:t_id>')
+def read_thread(t_board, t_id):
+    tpl=j_env.get_template('post.html')
+    OP = posts_coll.find_one( {'board' : t_board, 'thread':0, 'id': t_id } )
     if OP is None:
-        start_response('404 Not Found', [('Content-Type', 'text/html')])
-        return ["<h1>404</h1><br> A post with this id doesn't exist."]
-    #del OP['file']
-    replies = posts_coll.find( {'board':board, 'thread':t_id} ).sort('id',1)
-    ret=tpl.render(threads=[{'OP':OP, 'replies':replies}], mode={'thread':1, 'wrap':1}, board={'name':board}, config=conf)
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return [ret.encode('utf-8')]
-def board(environ, path, start_response):
+        return "<h1>404</h1><br> A thread with this id doesn't exist.", 404
+    replies = posts_coll.find( {'board':t_board, 'thread':t_id} ).sort('id',1)
+    ret=tpl.render(threads=[{'OP':OP, 'replies':replies}], mode={'thread':1, 'wrap':1}, board={'name':t_board}, config=conf)
+    return ret
+
+@app.route('/board/<board>')
+def board(board):
     tpl=j_env.get_template('post.html')
-    try:
-        board = path[1].decode('utf-8')
-    except (IndexError, ValueError, TypeError):
-        start_response('400 Bad Request', [('Content-Type', 'text/html')])
-        return ["Invalid board."]
     thr = list( posts_coll.find( {'board':board, 'thread':0} ).sort('id', -1).limit(15) )
     def repgen(tl):
         for i in tl:
             rep = posts_coll.find( {'board':board, 'thread': i['id']} ).sort('id', -1).limit(3)
             yield {'OP':i, 'replies':rep}
-    #dbg='||'.join(map(repr, l2))
     ret = tpl.render(b_threads=repgen(thr), mode={'board':1}, board={'name':board, 'anonymous':'Anone'}, config=conf)
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return [ret.encode('utf-8')]
+    return ret
 
 def myapp(environ, start_response):
-    #try:
-    #	request_body_size = int(environ.get('CONTENT_LENGTH', 0))
-    #except (ValueError):
-    #	request_body_size = 0
-    #request_body = environ['wsgi.input'].read(request_body_size)
-    #post = urlparse.parse_qs(request_body)
     path = environ['SCRIPT_NAME'].split('/')[2:]
     if len(path)==0 or path[0]=='search':
         return search1(environ, path, start_response)
@@ -114,4 +94,4 @@ if __name__ == '__main__':
         addr = (sys.argv[1], int(sys.argv[2]))
     else:
         addr = ('127.0.0.1', 9001)
-    WSGIServer(myapp, bindAddress=addr).run()
+    WSGIServer(app, bindAddress=addr).run()
