b563748f839d6885492c3e83084be13694c5c8f4
Sebacjusz <sovietmudkipz22@gmail.com>
2011-10-10 13:03:27 +0200

    use self-hosted jQuery, fix post preview path

--- static/rakachan.js
+++ static/rakachan.js
@@ -1,7 +1,7 @@
 
 function ppv(t, board,id){
 	$("<div id='preview' style='position: fixed'></div>").appendTo("body");
-	$('#preview').load('/alfa/post/' + board + '/' + id);
+	$('#preview').load('/post/' + board + '/' + id);
 	var position = $(t).position(); 
 	$('#preview').css('top', parseInt(position.top) - window.pageYOffset); 
 	i = parseInt(position.left) + parseInt($(t).css('width'));
